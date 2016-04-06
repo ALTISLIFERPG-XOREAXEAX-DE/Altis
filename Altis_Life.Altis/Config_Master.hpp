4aa6801f436c394eea3fce37f28b95fccf3dc987
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-04-06 20:35:39 +0200

    Mission Builder

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -119,14 +119,14 @@ class Life_Settings {
     impound_air = 850; //Price for impounding helicopters / planes
 
     /* Initial Bank Amount */
-    bank_cop = 30000; //Amount of cash on bank for new cops
-    bank_civ = 50000; //Amount of cash on bank for new civillians
-    bank_med = 70000; //Amount of cash on bank for new medics
+    bank_cop = 50000; //Amount of cash on bank for new cops
+    bank_civ = 100000; //Amount of cash on bank for new civillians
+    bank_med = 200000; //Amount of cash on bank for new medics
 
     /* Paycheck Amount */
     paycheck_cop = 500; //Payment for cops
     paycheck_civ = 1000; //Payment for civillians
-    paycheck_med = 1500; //Payment for medics
+    paycheck_med = 2000; //Payment for medics
 
     /* Federal Reserve settings */
     noatm_timer = 10; //Time in minutes that players won't be able to deposit his money after selling stolen gold
