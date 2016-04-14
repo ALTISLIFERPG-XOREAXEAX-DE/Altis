35fae2cd2ed51f5aee9faa3ff9811e247e935265
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-04-14 15:47:36 +0200

    bugs

--- Altis_Life.Altis/Config_vItems.hpp
+++ Altis_Life.Altis/Config_vItems.hpp
@@ -493,7 +493,7 @@ class VirtualItems {
         displayName = "STR_Item_HeroinP";
         weight = 2;
         buyPrice = 40000;
-        sellPrice = 28035; // HEROIN_PROCESSED_SELLPRICE
+        sellPrice = 22585; // HEROIN_PROCESSED_SELLPRICE
         illegal = true;
         edible = -1;
         icon = "icons\ico_heroinProcessed.paa";
@@ -516,7 +516,7 @@ class VirtualItems {
         displayName = "STR_Item_Marijuana";
         weight = 4;
         buyPrice = 20000;
-        sellPrice = 3815; // MARIJUANA_PROCESSED_SELLPRICE
+        sellPrice = 18403; // MARIJUANA_PROCESSED_SELLPRICE
         illegal = true;
         edible = 10;
         icon = "icons\ico_marijuana.paa";
@@ -539,7 +539,7 @@ class VirtualItems {
         displayName = "STR_Item_CocaineP";
         weight = 2;
         buyPrice = 40000;
-        sellPrice = 15673; // COCAINE_PROCESSED_SELLPRICE
+        sellPrice = 26601; // COCAINE_PROCESSED_SELLPRICE
         illegal = true;
         edible = 100;
         icon = "icons\ico_cocaineProcessed.paa";
